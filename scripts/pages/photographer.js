a4c6ee8708d97a822a58d5c233df2307abf4acb0
Audmes <102157665+Audmes@users.noreply.github.com>
2023-11-13 17:33:11 +0100

    Commit : 13/11/2023 - Fonctions relatives aux données - Réorganisation du CSS - Ajout des photos

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,63 @@
 //Mettre le code JavaScript lié à la page photographer.html
+let photographerId = window.location.search;
+photographerId = photographerId.slice(4);
+// console.log(photographerId);
+
+const getData = async () => {
+    // Récupère les datas des photographes
+    const response = await fetch('data/photographers.json');
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+const getPhotographerById = async (photographerId) => {
+    // Récupère les datas du photographe par son Id
+    const data = await getData();
+    let dataId = data[('photographers')];
+
+    return dataId.filter(
+        function(data) { return data.id == photographerId }
+    );
+}
+
+const getPhotographerMediaById = async (photographerId) => {
+    // Récupère les datas média du photographe par son Id
+    const data = await getData();
+    let mediaId = data[('media')];
+    return mediaId.filter(
+        function(data) { return data.photographerId == photographerId }
+    );
+}
+
+async function displayDataPhotographer(photographer) {
+    // Disposition des données du photographe
+    // const { name } = photographer[0];
+    const photographersSection = document.querySelector(".photograph-header");
+
+    // console.log({name});
+
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+}
+
+async function displayDataMedia(photographer, photographerMedia) {
+    // Disposition des données Media du photographe
+    const photographersSection = document.querySelector(".photographer-media_section");
+
+    photographerMedia.forEach((media) => {
+        const photographerModel = photographerMediaTemplate(photographer, media);
+        const userMediaDOM = photographerModel.getUserMediaDOM();
+        photographersSection.appendChild(userMediaDOM);
+    });
+}
+
+async function init(photographerId) {
+    // Récupère les datas du photographe
+    const photographer = await getPhotographerById(photographerId);
+    const photographerMedia = await getPhotographerMediaById(photographerId);
+    displayDataPhotographer(photographer[0]);
+    displayDataMedia(photographer[0], photographerMedia);
+}
+init(photographerId);
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,74 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
+    // console.log(data);
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+        const article = document.createElement( 'article');
+
+        const link = document.createElement('a');
+        const href = `photographer.html?id=${id}`;
+        link.setAttribute('href', href);
+        
+        const ariaLabel = `View Photographer Profile: ${name}`;
+        link.setAttribute('aria-label', ariaLabel);
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        const h3 = document.createElement('h3');
+        h3.textContent = city+','+' '+country;
+
+        const p = document.createElement('p');
+        p.textContent = tagline;
+
+        const pPrice = document.createElement('p');
+        pPrice.classList.add("price");
+        pPrice.textContent = price+'€/jour';
+
+        link.appendChild(img);
+        link.appendChild(h2);
+        link.appendChild(h3);
+        link.appendChild(p);
+        link.appendChild(pPrice);
+        article.appendChild(link);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { data, getUserCardDOM }
+}
+
+function photographerMediaTemplate(photographer, data) {
+    const { name } = photographer;
+    const { date, id, image, likes, photographerId, price, title } = data;
+    
+    // console.log(data);
+    // console.log(photographer);
+
+    const picture = `assets/photographers/${name}/${image}`;
+
+    function getUserMediaDOM() {
+        const article = document.createElement( 'article' );
+        
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", picture);
+
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = title;
+
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = likes;
+
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(h3);
+
+        return (article);
+    }
+    
+    return { date, id, image, likes, photographerId, price, title, getUserMediaDOM }
 }
