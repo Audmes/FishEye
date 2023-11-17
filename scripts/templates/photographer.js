18600f522ade6d1acdcc488f01ccc817d3f5f7ea
Audmes <102157665+Audmes@users.noreply.github.com>
2023-11-17 12:45:41 +0100

    Commit : 17/11/2023 - Ajout du Bouton Like sur la page : photographer.html

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -15,7 +15,8 @@ function photographerTemplate(data) {
         link.setAttribute('aria-label', ariaLabel);
 
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Image de ${name}`);
 
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
@@ -50,22 +51,47 @@ function photographerMediaTemplate(photographer, data) {
     // console.log(photographer);
 
     const picture = `assets/photographers/${name}/${image}`;
+    const svg = `assets/icons/heart.svg`;
+    // const video = `assets/photographers/${name}/${image}`;
 
     function getUserMediaDOM() {
+        // On crée l’élément article.
         const article = document.createElement( 'article' );
-        
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
 
+        // On crée l’élément img.
+        const img = document.createElement( 'img' );
+        // img.src = "data:image/png;base64," + base64JsonData;
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Image de ${title}`);
+        
+        const div = document.createElement('div');
+        div.classList.add("desc");
+        
+        // On crée l’élément h1.
         const h2 = document.createElement( 'h2' );
         h2.textContent = title;
 
-        const h3 = document.createElement( 'h3' );
-        h3.textContent = likes;
+        const divLike = document.createElement('div');
+        divLike.classList.add("wrap-like");
+
+        // On crée l’élément p.
+        const p = document.createElement( 'p' );
+        p.textContent = likes;
+
+        const heart = document.createElement('img');
+        heart.setAttribute('alt','Like button');
+        heart.setAttribute('width','21');
+        heart.setAttribute('height','24');
+        heart.setAttribute("src", svg);
+        // heart.setAttribute('id','heart');
+        heart.classList.add('button-heart');
 
         article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h3);
+        article.appendChild(div);
+        div.appendChild(h2);
+        div.appendChild(divLike);
+        divLike.appendChild(p);
+        divLike.appendChild(heart);
 
         return (article);
     }
